32776b53140fddb15561e9873c7689c2637fc1ed
Steve Wagner <ciroque@live.com>
2017-02-08 20:12:09 -0800

    Query Service hooked up to DataStore

--- src/main/scala/org/ciroque/animalia/services/QueryService.scala
+++ src/main/scala/org/ciroque/animalia/services/QueryService.scala
@@ -1,6 +1,21 @@
 package org.ciroque.animalia.services
 
+import org.ciroque.animalia.models.Fact
+import org.ciroque.animalia.persistence.DataStore
+
+import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
 trait QueryService {
 
-  def queryFacts() = ???
+  implicit val dataStore: DataStore
+
+  def query(query: Fact): Future[List[String]] = {
+    dataStore.query(query)
+  }
+
+  def queryCount(query: Fact): Future[Int] = {
+    dataStore.queryCount(query)
+  }
 }
--- src/test/scala/org/ciroque/animalia/services/QueryServiceTest.scala
+++ src/test/scala/org/ciroque/animalia/services/QueryServiceTest.scala
@@ -1,14 +1,66 @@
 package org.ciroque.animalia.services
 
+import org.ciroque.animalia.Any
+import org.ciroque.animalia.models.Fact
+import org.ciroque.animalia.persistence.DataStore
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.easymock.EasyMockSugar
+import org.scalatest.{BeforeAndAfterEach, FunSpec, Matchers}
+import org.easymock.EasyMock._
+
+import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class QueryServiceTest
   extends FunSpec
     with ScalaFutures
-    with Matchers {
+    with Matchers
+    with EasyMockSugar
+    with BeforeAndAfterEach {
+
+  val mockDataStore = mock[DataStore]
+
+  val service = new QueryService {
+    override implicit val dataStore: DataStore = mockDataStore
+  }
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockDataStore)
+  }
 
   describe("QueryService") {
+    describe("which") {
+      it("passes the call through to the DataStore") {
+        val expected = List("animal1", "animal2")
+        val query = Fact(Any.alphanumericString(), "isa", Any.alphanumericString())
+        expecting {
+          mockDataStore.query(query).andReturn(Future(expected))
+        }
+        whenExecuting(mockDataStore) {
+          whenReady(service.query(query)) {
+            animals: List[String] =>
+              animals shouldBe expected
+          }
+        }
+      }
+    }
 
+    describe("how-many") {
+      it("passes the call through to the DataStore") {
+        val expected = 9806
+        val query = Fact(Any.alphanumericString(), "isa", Any.alphanumericString())
+        expecting {
+          mockDataStore.queryCount(query).andReturn(Future(expected))
+        }
+        whenExecuting(mockDataStore) {
+          whenReady(service.queryCount(query)) {
+            animals: Int =>
+              animals shouldBe expected
+          }
+        }
+      }
+    }
   }
 }
